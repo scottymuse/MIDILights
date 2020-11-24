9e0663549f300424037c2fe372028141ede17d66
scottymuse <scottymuse@gmail.com>
2020-11-24 13:18:59 -0700

    Adding rainbow

--- MIDILights.ino
+++ MIDILights.ino
@@ -2,6 +2,11 @@
 #include <MIDI.h>
 
 #define NUMPIXELS 60
+#define OFFSET 36
+
+struct {
+  byte color;
+}lights[NUMPIXELS];
 
 //#define DATAPIN 11  // 11 and 13 are the SPI pins on the UNO. Supposedly faster
 //#define CLOCKPIN 13 // and not needed in the constructor call
@@ -11,20 +16,34 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 
 void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
-  if (pitch-30 >= 0 && pitch-30 < NUMPIXELS)
-  {
-    strip.setPixelColor(pitch-30, 0x880000);
+  byte light = pitch-OFFSET;
+  switch(channel) {
+    case 0x01 :
+      rainbow_note(light);
+      break;
   }
-  strip.show();
+/*
+  if (light >= 0 && light < NUMPIXELS)
+  {
+    strip.setPixelColor(light, 0x880000);
+  }
+*/
 }
 
 void handleNoteOff(byte channel, byte pitch, byte velocity)
 {
-  if (pitch-30 >= 0 && pitch-30 < NUMPIXELS)
-  {
-    strip.setPixelColor(pitch-30, 0);
+  byte light = pitch-OFFSET;
+  switch(channel) {
+    case 0x01 :
+      rainbow_note_off(light);
+      break;
   }
-  strip.show();
+/*  
+  if (light >= 0 && light < NUMPIXELS)
+  {
+    strip.setPixelColor(light, 0);
+  }
+*/
 }
 
 void setup()
@@ -40,4 +59,31 @@ void setup()
 void loop()
 {
   MIDI.read();
+  strip.show();
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+void rainbow_note(byte light) {
+  static byte color = 0;
+  strip.setPixelColor(light, Wheel(color++));
+  if (color > 255) {
+    color = 0;
+  }
+}
+
+void rainbow_note_off(byte light) {
+  strip.setPixelColor(light, 0);
 }
